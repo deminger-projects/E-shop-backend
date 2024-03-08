0d2f2e7f57a4271d935483ee69e6446570d7c1a0
pikosoZBrna <dominikjan1@gmail.com>
2024-03-08 16:40:55 +0100

    node

--- routes.ts
+++ routes.ts
@@ -225,6 +225,19 @@ export const router = Router()
 
 
 
+
+
+
+
+
+
+
+
+  
+
+
+
+
   router.post('/test_request', try_catch(async function (req: Request, res: Response) {   
 
     var data: any = await Promise.all([write_json(["SELECT products.id, products.name as product_name, products.price, DATE_FORMAT(products.add_date, '%Y-%m-%d') as add_date, products.discount, products.description, product_images.image_url as 'url', collections.id as collection_id, collections.name as collection_name from products left join collections on collections.id = products.collection_id join product_images on product_images.product_id = products.id WHERE products.status = 'Active' AND product_images.image_url like '%_main.%';", 
