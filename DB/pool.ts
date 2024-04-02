8c54fb02c28c5657336d9594a2c7f2d7cd1b5f27
pikosoZBrna <dominikjan1@gmail.com>
2024-04-03 00:18:13 +0200

    node

--- DB/pool.ts
+++ DB/pool.ts
@@ -1,30 +1,10 @@
 import * as mysql from "mysql"
 
-// export const pool = mysql.createPool({
-//     host: 'viaduct.proxy.rlwy.net',
-//     user: 'root',
-//     password: '5c5hbH22f3dddAF-D23D6-545CGbGdf2',
-//     database: 'railway',   
-//     port: 43545,
-//     connectionLimit: 10,
-//     multipleStatements: false
-//   })
-
-
   export const pool = mysql.createPool({
-    host: 'srv1342.hstgr.io',
-    user: 'u976476949_pepa',
-    password: '4oVEqeZ!',
-    database: 'u976476949_joynda_shop',   
+    host: 'monorail.proxy.rlwy.net',
+    user: 'root',
+    password: 'kNCTPsadaPxbNEUNROgvqaZjNNkHtIdE',
+    database: 'railway',   
     connectionLimit: 10,
     multipleStatements: false
   })
-
-// export const pool = mysql.createPool({ //local
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'eshop_2022',   
-//   connectionLimit: 10,
-//   multipleStatements: false
-// }) 
