abb38202a3358a0c194827d2353342289c3a698b
pikosoZBrna <dominikjan1@gmail.com>
2024-09-30 17:16:58 +0200

    server

--- controller/file_handlers/savers/save_files.ts
+++ controller/file_handlers/savers/save_files.ts
@@ -36,7 +36,7 @@ export default async function save_files(path: string, files: FileArray){
 
         var test_name = "products_" + file.name
 
-        file.mv("/image_storage/" + test_name)
+        file.mv("/app/image_storage/" + test_name)
       }
 
       await Promise.all(prom)
