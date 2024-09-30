7751acf91ef0939a48f65e145073453fbe60cdf4
pikosoZBrna <dominikjan1@gmail.com>
2024-09-30 16:48:09 +0200

    server

--- controller/file_handlers/savers/save_files.ts
+++ controller/file_handlers/savers/save_files.ts
@@ -15,12 +15,12 @@ export default async function save_files(path: string, files: FileArray, record_
       fs.mkdirSync(path)
     }
 
-    if(!fs.existsSync("/image_storage/" + folder + "/")){
+    if(!fs.existsSync("./image_storage/" + folder + "/")){
       fs.mkdirSync("/image_storage/" + folder + "/")
     }
 
-    if(!fs.existsSync("/image_storage/" + folder + "/" + record_id + "/")){
-      fs.mkdirSync("/image_storage/" + folder + "/" + record_id + "/")
+    if(!fs.existsSync("./image_storage/" + folder + "/" + record_id + "/")){
+      fs.mkdirSync("./image_storage/" + folder + "/" + record_id + "/")
     }
       
 
