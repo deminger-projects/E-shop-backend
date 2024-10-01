05760d415fabbc269c78db364b2849cdffc4100b
pikosoZBrna <dominikjan1@gmail.com>
2024-10-01 13:54:46 +0200

    server

--- controller/file_handlers/savers/save_files_to_volume.ts
+++ controller/file_handlers/savers/save_files_to_volume.ts
@@ -18,40 +18,50 @@ export default async function save_files_to_volume(files: FileArray, folder: str
     sharp.cache(false);
 
     if(multiple_files){
-        for(let file of multiple_files){
-            //sharp(path + file)
-            sharp("/image_storage/" + folder + "/" + record_id + "/" + file)
-            .jpeg()
-            .jpeg({ quality: 100 })
-            .jpeg({ progressive: true })
-            .resize(200, 200)  
-            .toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-            // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-            //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-                if (err) {
-                console.error(err);
-                }else{
-                    fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + file)
-                }
-            }) 
+        for(var file of multiple_files){
+            await file.mv("/image_storage/" + folder + "/" + record_id + "/" + file.name)
         }
     }
 
     if(single_file){
-        sharp("/image_storage/" + folder + "/" + record_id + "/" + single_file)
-        .jpeg()
-        .jpeg({ quality: 100 })
-        .jpeg({ progressive: true })
-        .resize(200, 200)  
-        .toFile("/image_storage/" + folder + "/" + record_id + "/" + single_file, (err: Error, info: any) => {
-        // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-        //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-            if (err) {
-            console.error(err);
-            }else{
-                fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + single_file)
-            }
-        }) 
+        await single_file.mv("/image_storage/" + folder + "/" + record_id + "/" + single_file.name)
     }
+
+    // if(multiple_files){
+    //     for(let file of multiple_files){
+    //         //sharp(path + file)
+    //         sharp("/image_storage/" + folder + "/" + record_id + "/" + file)
+    //         .jpeg()
+    //         .jpeg({ quality: 100 })
+    //         .jpeg({ progressive: true })
+    //         .resize(200, 200)  
+    //         .toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //         // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //         //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //             if (err) {
+    //             console.error(err);
+    //             }else{
+    //                 fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + file)
+    //             }
+    //         }) 
+    //     }
+    // }
+
+    // if(single_file){
+    //     sharp("/image_storage/" + folder + "/" + record_id + "/" + single_file)
+    //     .jpeg()
+    //     .jpeg({ quality: 100 })
+    //     .jpeg({ progressive: true })
+    //     .resize(200, 200)  
+    //     .toFile("/image_storage/" + folder + "/" + record_id + "/" + single_file, (err: Error, info: any) => {
+    //     // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //     //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //         if (err) {
+    //         console.error(err);
+    //         }else{
+    //             fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + single_file)
+    //         }
+    //     }) 
+    // }
 
 }
