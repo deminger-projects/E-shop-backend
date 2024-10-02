13f692af7b658e4a6459f54d96e0650d8f6efca4
pikosoZBrna <dominikjan1@gmail.com>
2024-10-02 15:07:55 +0200

    server

--- controller/file_handlers/savers/save_files_to_volume.ts
+++ controller/file_handlers/savers/save_files_to_volume.ts
@@ -21,15 +21,15 @@ export default async function save_files_to_volume(files: FileArray, folder: str
 
     sharp.cache(false);
 
-    // if(multiple_files){
-    //     for(var file of multiple_files){
-    //         await file.mv(storage_name + folder + "/" + record_id + "/" + file.name)
-    //     }
-    // }
+    if(multiple_files){
+        for(var file of multiple_files){
+            await file.mv(storage_name + folder + "/" + record_id + "/" + file.name)
+        }
+    }
 
-    // if(single_file){
-    //     await single_file.mv(storage_name + folder + "/" + record_id + "/" + single_file.name)
-    // }
+    if(single_file){
+        await single_file.mv(storage_name + folder + "/" + record_id + "/" + single_file.name)
+    }
 
 
 
@@ -45,41 +45,41 @@ export default async function save_files_to_volume(files: FileArray, folder: str
 
 
 
-    if(multiple_files){
-        for(let file of multiple_files){
-            //sharp(path + file)
-            sharp("/image_storage/" + folder + "/" + record_id + "/" + file)
-            .jpeg()
-            .jpeg({ quality: 100 })
-            .jpeg({ progressive: true })
-            .resize(200, 200)  
-            .toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-            // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-            //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-                if (err) {
-                console.error(err);
-                }else{
-                    fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + file)
-                }
-            }) 
-        }
-    }
+    // if(multiple_files){
+    //     for(let file of multiple_files){
+    //         //sharp(path + file)
+    //         sharp("/image_storage/" + folder + "/" + record_id + "/" + file)
+    //         .jpeg()
+    //         .jpeg({ quality: 100 })
+    //         .jpeg({ progressive: true })
+    //         .resize(200, 200)  
+    //         .toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //         // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //         //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //             if (err) {
+    //             console.error(err);
+    //             }else{
+    //                 fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + file)
+    //             }
+    //         }) 
+    //     }
+    // }
 
-    if(single_file){
-        sharp("/image_storage/" + folder + "/" + record_id + "/" + single_file)
-        .jpeg()
-        .jpeg({ quality: 100 })
-        .jpeg({ progressive: true })
-        .resize(200, 200)  
-        .toFile("/image_storage/" + folder + "/" + record_id + "/" + single_file, (err: Error, info: any) => {
-        // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-        //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
-            if (err) {
-            console.error(err);
-            }else{
-                fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + single_file)
-            }
-        }) 
-    }
+    // if(single_file){
+    //     sharp("/image_storage/" + folder + "/" + record_id + "/" + single_file)
+    //     .jpeg()
+    //     .jpeg({ quality: 100 })
+    //     .jpeg({ progressive: true })
+    //     .resize(200, 200)  
+    //     .toFile("/image_storage/" + folder + "/" + record_id + "/" + single_file, (err: Error, info: any) => {
+    //     // .toFile("./image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //     //.toFile("/image_storage/" + folder + "/" + record_id + "/" + file, (err: Error, info: any) => {
+    //         if (err) {
+    //         console.error(err);
+    //         }else{
+    //             fs.unlinkSync("/image_storage/" + folder + "/" + record_id + "/" + single_file)
+    //         }
+    //     }) 
+    // }
 
 }
